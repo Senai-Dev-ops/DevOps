16b08d362526fe1f88aced59909735440a651f18
Kelvin Novais <83086622+KelvinNovais@users.noreply.github.com>
2022-03-14 09:47:50 -0300

    Update SetUp.sh

--- SetUp.sh
+++ SetUp.sh
@@ -18,16 +18,15 @@ DOCKER_IMAGES=(
 	nginx
 	postgres
 	dpage/pgadmin4
-	maven
 )
 
 # Users
 USERS=(
+	kelvinra
 	nascimento
 	garcia
 	caetano
 	ozorio
-	novais
 	diogo
 	rondon
 	severiano
@@ -53,7 +52,19 @@ for user in ${USERS[@]}; do
 	sudo adduser $user
 	sudo usermod -aG sudo $user
 done
-sudo usermod -aG libvirt novais
+sudo usermod -aG libvirt ${USERS[0]}
+
+# Installing NodeJS and npm
+# Removing possible old versions
+sudo apt-get remove --purge nodejs
+sudo apt-get remove --purge npm
+sudo rm -rf ~/.npm
+sudo rm -rf /usr/local/lib/node_modules
+cd /tmp/
+curl -0 -L https://npmjs.org/install.sh | sudo sh
+
+# Installing Heroku
+sudo npm install -g heroku
 ###################################################################
 
 # DOCKER ##########################################################
@@ -65,9 +76,21 @@ done
 # POSTGRESQL AND PGADMIN4
 # Creating a network for the PostgreSQL and PgAdmin containers
 sudo docker network create --driver bridge postgres-network
-
 # Creating a PostgreSQL container
-read -p 'Enter the Postgres password: ' POSTGRES_PASSWD
+PSQL_PASSWD_LOCK=1
+while [ "$PSQL_PASSWD_LOCK" -eq "1" ]
+do
+	read -s -p 'Enter the Postgres password: ' POSTGRES_PASSWD
+	echo
+	read -s -p 'Confirm your Postgres password: ' POSTGRES_PASSWD_CONFIRM
+	echo
+	if [ $POSTGRES_PASSWD == $POSTGRES_PASSWD_CONFIRM ] && [ ! $POSTGRES_PASSWD == "" ] ; then
+		echo "Passwords did match!"
+		PSQL_PASSWD_LOCK=0
+	else
+		echo "Invalid passwords, please retype:"
+	fi
+done
 sudo docker run --name bc-postgres --network=postgres-network -e "POSTGRES_PASSWORD=$POSTGRES_PASSWD" -p $PORT:$PORT -v /home/$USER/Desenvolvimento/PostgreSQL:/var/lib/postgresql/data -d postgres
 
 # Creating a PgAdmin container
@@ -77,6 +100,14 @@ docker run --name bc-pgadmin --network=postgres-network -p 15432:80 -e "PGADMIN_
 # URL: https://localhost:15432/	OR	http://0.0.0.0:15432/
 
 # Font: https://imasters.com.br/banco-de-dados/postgresql-docker-executando-uma-instancia-e-o-pgadmin-4-partir-de-containers
+
+# BACKEND
+git clone https://github.com/Senai-Dev-ops/Back.git
+cd /Back/backend/
+mvn clean package
+mvn spring-boot:build-image
+sudo docker run -it -p 5000:8080 docker.io/library/dsvendas:0.0.1-SNAPSHOT
+cd /tmp/
 ###################################################################
 
 # FINISHING #######################################################
